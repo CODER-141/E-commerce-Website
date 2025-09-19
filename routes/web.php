dd23f7067cbfc35f4a785da3a1ecffea944b7ae6
CODER-141 <thimiraushan2000@gmail.com>
2025-09-19 13:32:16 +0530

    Create Admin Controller & Middleware to prevent unauthorized access
    Create Admin Controller & Middleware to prevent unauthorized access

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -8,12 +9,18 @@ Route::get('/', function () {
     return view('index');
 })->name('index');
 
+
 Route::get('/dashboard', [UserController::class,'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth','admin'])->group(function () {
+Route::get('/test_admin', [AdminController::class, 'test_admin'])->name('admin.test');
+
 });
 
 require __DIR__.'/auth.php';
